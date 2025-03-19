9982628e587da6b1f9ff2403ffcfd0d37ff84942
abdulbasid <abdulbasidhussain9@gmail.com>
2025-03-19 22:39:45 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
 
-module.exports = {
+export default {
     eslint: {
       ignoreDuringBuilds: true,
     },
