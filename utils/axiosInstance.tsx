75d91c0e534ea88b5433cb96edec55df968e892e
abdulbasid <abdulbasidhussain9@gmail.com>
2025-06-29 20:42:57 +0100

    update

--- utils/axiosInstance.tsx
+++ utils/axiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://istiqamauni-1.onrender.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://istiqamauni-1.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
